5878bba28fb1c493951dcb58d4a60c8b223c6f8b
SanGryg <165597103+SanGryg@users.noreply.github.com>
2024-10-17 16:12:07 +0200

    Add files via upload

--- Box.java
+++ Box.java
@@ -3,30 +3,31 @@ public class Box {
     double width;
     double height;
 
-    Box(){
+    Box() {
         this(10);
     }
 
-    Box(Box another){
+    Box(Box another) {
         this(another.length, another.width, another.height);
     }
 
-    Box(double length, double width, double height){
+    Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
-    Box(double size){
-       this(size, size, size);
+    Box(double size) {
+        this(size, size, size);
     }
 
-    Box copy(){
+    Box copy() {
         return new Box(this.length, this.width, this.height);
 
     }
-    Box increase(){
-        return new Box(this.length*2, this.width*2, this.height*2);
+
+    Box increase() {
+        return new Box(this.length * 2, this.width * 2, this.height * 2);
     }
 
     void setDimensions(double length, double width, double height) {
@@ -35,14 +36,14 @@ public class Box {
         this.height = height;
     }
 
-    void compare(Box another){
+    void compare(Box another) {
         double currentVolume = getVolume();
         double anotherVolume = another.getVolume();
-        if(currentVolume > anotherVolume){
+        if (currentVolume > anotherVolume) {
             System.out.println("Current > Another");
-        }else if(currentVolume < anotherVolume){
+        } else if (currentVolume < anotherVolume) {
             System.out.println("Current < Another");
-        }else {
+        } else {
             System.out.println("Current == Another");
         }
     }
@@ -50,6 +51,7 @@ public class Box {
     double getVolume() {
         return length * width * height;
     }
+
     void showVolume() {
         System.out.println(getVolume());
     }
